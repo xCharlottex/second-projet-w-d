ea8d5b4cb80f816a388f03e2fbaaef4c1bbe96de
ChaCoutinot <charlotte.coutinot@hotmail.com>
2022-06-30 15:42:45 +0200

    my_project_symfony

--- src/Controller/NewController.php
+++ src/Controller/NewController.php
@@ -3,15 +3,21 @@
 namespace App\Controller;
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class NewController {
+class NewController extends AbstractController {
 
     /**
      * @Route("/", name="hello")
      */
     public function hello(){
-        return new Response('coucou toi');
+
+        $name = 'Cha';
+
+       return $this->render('hello.html.twig', [
+           'name' => $name
+       ]);
     }
 }
