9877941c782e22ca59340f57e4744951e59436a4
ChaCoutinot <charlotte.coutinot@hotmail.com>
2022-06-30 14:09:54 +0200

    second projet w/d

---
+++ src/Controller/NewController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class NewController {
+
+    /**
+     * @Route("/", name="hello")
+     */
+    public function hello(){
+        return new Response('coucou toi');
+    }
+}
